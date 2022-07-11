3bc367640e9e27e690b27ffd9efca29995968ae4
Reece Bayley <rlweirdddude@gmail.com>
2022-07-11 14:36:16 +0200

    Fully implemented movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -10,13 +10,13 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
-		velocity.x += 1
+		velocity.y -= 1
 	if Input.is_action_pressed("move_down"):
-		velocity.x -= 1
+		velocity.y += 1
 	if Input.is_action_pressed("move_right"):
-		velocity.y += 1
+		velocity.x -= 1
 	if Input.is_action_pressed("move_left"):
-		velocity.y += 1
+		velocity.x += 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -24,6 +24,9 @@ func _process(delta):
 	else:
 		$AnimatedSprite.stop()
 	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
